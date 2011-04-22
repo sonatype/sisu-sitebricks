5311d729ab97015a72b17850531ceded31788c07
Jason van Zyl <jason@sonatype.com>
2011-04-22 14:32:38 -0400

    o moved the extjs classes to their own project

--- src/main/java/org/sonatype/sisu/sitebricks/form/ColumnDefinition.java
+++
@@ -1,6 +0,0 @@
-package org.sonatype.sisu.sitebricks.form;
-
-public interface ColumnDefinition
-{
-
-}
--- src/main/java/org/sonatype/sisu/sitebricks/form/FormData.java
+++
@@ -1,6 +0,0 @@
-package org.sonatype.sisu.sitebricks.form;
-
-public @interface FormData
-{
-    String value() default "create";
-}
